b3cbac91f248b455549a501ddb0b0c206f0fa112
Talgat <accurate19@mail.ru>
2024-08-28 12:02:34 +0300

    Last version

---
+++ src/main/java/web/DAO/UserDAO.java
@@ -0,0 +1,60 @@
+package web.DAO;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import web.model.User;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.List;
+
+@Repository
+public class UserDAO {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Transactional
+    public List<User> getAllUsers() {
+        return entityManager.createQuery("select u from User u", User.class)
+                .getResultList();
+    }
+
+    @Transactional
+    public User getUserById(int id) {
+        TypedQuery<User> query = entityManager.createQuery(
+                "select u from User u where u.id = :id", User.class);
+        query.setParameter("id", id);
+        return query.getResultList().stream().findAny().orElse(null);
+    }
+
+    @Transactional
+    public void addUser(User user) {
+        entityManager.persist(user);
+    }
+
+    @Transactional
+    public void updateUser(int id, User updatedUser) {
+        TypedQuery<User> query = entityManager.createQuery(
+                "select u from User u where u.id = :id", User.class
+        );
+        query.setParameter("id", id);
+        User user = query.getSingleResult();
+        user.setName(updatedUser.getName());
+        user.setSurname(updatedUser.getSurname());
+        user.setAge(updatedUser.getAge());
+        entityManager.merge(user);
+    }
+
+    @Transactional
+    public void deleteUser(int id) {
+        entityManager.remove(getUserById(id));
+    }
+
+    }
--- src/main/java/web/config/AppInit.java
+++ src/main/java/web/config/AppInit.java
@@ -1,6 +1,10 @@
 package web.config;
 
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 
 public class AppInit extends AbstractAnnotationConfigDispatcherServletInitializer {
 
@@ -17,6 +21,20 @@ public class AppInit extends AbstractAnnotationConfigDispatcherServletInitialize
         return new Class<?>[]{
                 WebConfig.class
         };
+    }
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        super.onStartup(servletContext);
+        registerHiddenFieldFilter(servletContext);
+    }
+
+    private void registerHiddenFieldFilter(ServletContext servletContext) {
+        servletContext
+                .addFilter("hiddenHttpMethodFilter",
+                        new HiddenHttpMethodFilter())
+                .addMappingForUrlPatterns(
+                        null, true, "/*");
     }
 
 
